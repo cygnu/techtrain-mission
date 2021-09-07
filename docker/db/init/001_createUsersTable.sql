8647ef6e2bec73054773d8c28d784e06effd190c
cygnu <crucia8@icloud.com>
2021-09-08 02:39:30 +0900

    feat: Add 'token' and update in docker/db/init/001_createUsersTable.sql

--- docker/db/init/001_createUsersTable.sql
+++ docker/db/init/001_createUsersTable.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
     `id`         INT(12) NOT NULL AUTO_INCREMENT,
-    `name`       VARCHAR(255) NOT NULL,
+    `name`       VARCHAR(50) NOT NULL,
+    `token`      VARCHAR(50) NOT NULL,
     `created_at` DATETIME NOT NULL,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
