4b5588a539a82bb23178d96ff3b7ba792d15d080
cygnu <crucia8@icloud.com>
2021-10-02 16:39:43 +0900

    feat: Implement main.go

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	ID   int
+	Name string
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	db, err := sql.Open("mysql", "root:pass@tcp(127.0.0.1:3306)/sample_db")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	var user User
+
+	err = db.QueryRow("SELECT * FROM users WHERE id = ?", 1).Scan(&user.ID, &user.Name)
+	switch {
+	case err == sql.ErrNoRows:
+		fmt.Println("レコードが存在しません")
+	case err != nil:
+		panic(err.Error())
+	default:
+		fmt.Println(user.ID, user.Name)
+	}
 }
